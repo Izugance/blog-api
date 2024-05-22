2e4158517c187a11744d7ede605620af2e1a210b
Praise Izuagie <izuagiepraise@gmail.com>
2024-05-22 22:21:52 +0100

    Fix: export 'up' and 'down' functions

--- migrations-src/seeders/20240501154524-test-users.js
+++ migrations-src/seeders/20240501154524-test-users.js
@@ -1,19 +1,23 @@
 "use strict";
 
-const up = async (queryInterface, Sequelize) => {
-  let users = [];
-  for (let i = 1; i <= 3; i++) {
-    users.push({
-      firstName: "Test" + i,
-      lastName: "User" + i,
-      email: "test" + i + "@test.com",
-      username: "test" + i,
-      password: "test",
-    });
-  }
-  await queryInterface.bulkInsert("Users", users);
-};
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    let users = [];
+    for (let i = 1; i <= 3; i++) {
+      users.push({
+        firstName: "Test" + i,
+        lastName: "User" + i,
+        email: "test" + i + "@test.com",
+        username: "test" + i,
+        password: "test",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+    await queryInterface.bulkInsert("Users", users);
+  },
 
-const down = async (queryInterface, Sequelize) => {
-  await queryInterface.bulkDelete("Users", null);
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null);
+  },
 };
